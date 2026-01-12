b0afd43d1b27fd5874eb47d96a6c1a74c5a00076
Yongho Kim <fillmemory@gmail.com>
2026-01-12 18:16:44 +0900

    feat: add initial Wisckey header and source files for key-value store implementation

---
+++ c/src/wisckey.h
@@ -0,0 +1,12 @@
+#ifndef FLINTDB_WISCKEY_H
+#define FLINTDB_WISCKEY_H
+
+#include "types.h"
+
+// TODO: Implement Wisckey key-value store structure and functions
+
+struct wisckey {
+    // Placeholder for Wisckey internal data structures
+};
+
+#endif // FLINTDB_WISCKEY_H
